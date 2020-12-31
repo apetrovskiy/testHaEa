c6b358d7ddd5f746b8c4a06fb91e86d51fd1c248
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-12-31 04:50:44 +0300

    Update solution3.py

--- src/main/algorithms/EasySumSetProblem/solution3.py
+++ src/main/algorithms/EasySumSetProblem/solution3.py
@@ -8,25 +8,34 @@ print('Hi, %s.' % name)         # Writing output to STDOUT
 '''
 
 # Write your code here
-from typing import List
+from typing import List, Set
 
 
-def calculate_item(c: int, list_a: List[int]) -> str:
-    index = 0
-    b = -1
-    while b < 0:
-        if c > list_a[index]:
-            return str(c - list_a[index])
-    return '0'
+# def calculate_item(c: int, list_a: List[int]) -> str:
+#     index = 0
+#     b = -1
+#     while b < 0:
+#         if c > list_a[index]:
+#             return str(c - list_a[index])
+#     return '0'
 
 
-def calculate_b(list_a: List[int], list_c: List[int]) -> List[str]:
-    return set([calculate_item(c, list_a) for c in list_c])
+def calculate_b(list_a: List[int], list_c: List[int]) -> Set[str]:
+    # return set([calculate_item(c, list_a) for c in list_c])
+    result_list = []
+    print(len(list_c))
+    for index in range(len(list_c)):
+        candidate = list_c[index] - list_a[index]
+        if candidate in result_list:
+            continue
+        else:
+            result_list.append(candidate)
+    return set([str(x) for x in result_list])
 
 
-length_of_a = int(input())
-list_a = [int(x) for x in input().split(' ')]
-length_of_c = int(input())
-list_c = [int(x) for x in input().split(' ')]
-result = calculate_b(list_a, list_c)
-print(' '.join(result))
+# length_of_a = int(input())
+# list_a = [int(x) for x in input().split(' ')]
+# length_of_c = int(input())
+# list_c = [int(x) for x in input().split(' ')]
+# result = calculate_b(list_a, list_c)
+# print(' '.join(result))
