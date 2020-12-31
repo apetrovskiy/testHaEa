f6a3ac43d993465c6d8a2a84d3c548ab1407dc82
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-12-31 05:36:01 +0300

    Update test_easy_sum_set.py

--- src/test/algorithms/EasySumSetProblem/test_easy_sum_set.py
+++ src/test/algorithms/EasySumSetProblem/test_easy_sum_set.py
@@ -4,10 +4,14 @@ from src.main.algorithms.EasySumSetProblem.solution3 import calculate_b
 
 
 test_data = [
-    ([1, 2], [3, 4, 5], ['2', '3']),([3],[5,6],['2','3']),([4,3,5,1],[3,5,6,7,8,9],['2','4']),([1,3,2,5,4],[2,3,4,5,6],['1'])
+    ([1, 2], [3, 4, 5], ['2', '3']),
+    ([3], [5, 6], ['2', '3']),
+    ([4, 3, 5, 1], [3, 5, 6, 7, 8, 9], ['2', '4']),
+    ([1, 3, 2, 5, 4], [2, 3, 4, 5, 6], ['1'])
 ]
 
 
 @pytest.mark.parametrize("array_a,array_c,array_b", test_data)
 def test_easy_sum_set(array_a: List[int], array_c: List[int], array_b: List[str]):
-    assert len(array_b) == len(calculate_b(array_a, array_c)) assert all([a == b for a, b in zip(array_b, calculate_b(array_a, array_c))]
+    assert len(array_b) == len(calculate_b(array_a, array_c))
+    assert all([a == b for a, b in zip(array_b, calculate_b(array_a, array_c))])
