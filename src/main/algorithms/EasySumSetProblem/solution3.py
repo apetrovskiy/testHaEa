a2a83386a837c9ac38ab5dec6921f7d9756857ae
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-12-31 05:29:10 +0300

    further

--- src/main/algorithms/EasySumSetProblem/solution3.py
+++ src/main/algorithms/EasySumSetProblem/solution3.py
@@ -11,27 +11,28 @@ print('Hi, %s.' % name)         # Writing output to STDOUT
 from typing import List, Set
 
 
-# def calculate_item(c: int, list_a: List[int]) -> str:
-#     index = 0
-#     b = -1
-#     while b < 0:
-#         if c > list_a[index]:
-#             return str(c - list_a[index])
-#     return '0'
-
-
 def calculate_b(list_a: List[int], list_c: List[int]) -> Set[str]:
-    # return set([calculate_item(c, list_a) for c in list_c])
-    result_list = []
-    print(len(list_c))
-    for index_c in range(len(list_c)):
-        for index_a in range(len(list_a)):
-            candidate = list_c[index_c] - list_a[index_a]
-            if candidate in result_list:
-                continue
-            else:
+    result_list = [list_c[0] - list_a[0]]
+    for index_c in range(1, len(list_c)):
+        # print(index_c)
+        # print(f"candidates are {[a + b for a in list_a for b in result_list]}")
+        # print(f"not in? {list_c[index_c] not in [a + b for a in list_a for b in result_list]}")
+        if list_c[index_c] not in [a + b for a in list_a for b in result_list]:
+            for index_a in range(len(list_a)):
+                candidate = list_c[index_c] - list_a[index_a]
                 result_list.append(candidate)
                 break
+            # candidate = min([a for a in list_a if list_c[index_c] > a])
+            # print(f"candidate = {candidate}")
+            # result_list.append(candidate)
+            # for index_a in range(len(list_a)):
+            #     candidate = list_c[index_c] - list_a[index_a]
+            #     if candidate in result_list:
+            #         continue
+            #     else:
+            #         result_list.append(candidate)
+            #         break
+    result_list.sort()
     return set([str(x) for x in result_list])
 
 
