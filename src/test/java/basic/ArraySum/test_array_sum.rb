88554dd954aecec09f7f58471a3c20ab634aee11
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-05 02:35:51 +0300

    Update test_array_sum.rb

--- src/test/java/basic/ArraySum/test_array_sum.rb
+++ src/test/java/basic/ArraySum/test_array_sum.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'rspec/autorun'
 require 'rubygems'
-# require 'minitest/autorun'
+require 'minitest/autorun'
 require_relative '../../../../main/java/basic/ArraySum/solution_array_sum'
 
 RSpec.describe 'array sum' do
@@ -18,6 +18,17 @@ RSpec.describe 'array sum' do
         expect(actual_result).to be == expected_output
         expect(solution_array_sum(input)).to eql expected_output
       end
+    end
+  end
+end
+
+class ArraySumTests < MiniTest::Unit::TestCase
+  [
+    [[1_000_000_001, 1_000_000_002, 1_000_000_003, 1_000_000_004, 1_000_000_005], 5_000_000_015],
+    [[1_001_458_909, 1_004_570_889, 1_007_019_111, 1_003_302_837, 1_002_514_638, 1_006_431_461, 1_002_575_010, 1_007_514_041, 1_007_548_981, 1_004_402_249], 10_047_338_126]
+  ].each do |input, expected_output|
+    define_method("test_output_is_#{expected_output}") do
+      assert_equal(expected_output, solution_array_sum(input))
     end
   end
 end
