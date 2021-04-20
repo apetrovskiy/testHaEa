17e83f4a770768268731dd9c6fb7bc4ce2f1147e
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-20 18:50:39 +0300

    updates

---
+++ testHaEa_suite_test.go
@@ -0,0 +1,13 @@
+package testHaEa_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestTestCoWa(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "TestHaEa Suite")
+}
